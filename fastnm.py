0a61be6a347fbf480036559bc171e7a0ffea4a00
Augusto Florentino <144605929+afiden@users.noreply.github.com>
2023-12-29 15:02:19 -0300

    Código completo V. 291223

---
+++ fastnm.py
@@ -0,0 +1,18 @@
+import subprocess
+
+def nmap_scan(ip):
+    initial_scan_cmd = f'nmap {ip} -p- --open'
+    initial_scan_result = subprocess.run(initial_scan_cmd, shell=True, text=True, capture_output=True)
+    open_ports = [line.split('/')[0] for line in initial_scan_result.stdout.splitlines() if 'open' in line]
+  
+    if open_ports:
+        detailed_scan_cmd = f'nmap {ip} -p {",".join(open_ports)} -sV'
+        detailed_scan_result = subprocess.run(detailed_scan_cmd, shell=True, text=True, capture_output=True)
+        return detailed_scan_result.stdout
+
+    return "Nenhuma porta aberta identificada."
+
+# Acrescente o endereço IP que deseja escanear
+ip_alvo = '[Endereço IP que você quer Scanear]'
+resultado_scan = nmap_scan(ip_alvo)
+print(resultado_scan)
