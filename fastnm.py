5bc8041012da9c88b1a0f952d899436f79c7679b
Augusto Florentino <144605929+afiden@users.noreply.github.com>
2024-01-30 10:46:13 -0300

    FastMap V 300124

--- fastnm.py
+++ fastnm.py
@@ -1,18 +1,37 @@
-import subprocess
+import nmap
+import os
 
-def nmap_scan(ip):
-    initial_scan_cmd = f'nmap {ip} -p- --open -T5 -Pn'
-    initial_scan_result = subprocess.run(initial_scan_cmd, shell=True, text=True, capture_output=True)
-    open_ports = [line.split('/')[0] for line in initial_scan_result.stdout.splitlines() if 'open' in line]
-  
-    if open_ports:
-        detailed_scan_cmd = f'nmap {ip} -p {",".join(open_ports)} -sV'
-        detailed_scan_result = subprocess.run(detailed_scan_cmd, shell=True, text=True, capture_output=True)
-        return detailed_scan_result.stdout
 
-    return "Nenhuma porta aberta identificada."
+def port_scan(target):
+    nm = nmap.PortScanner()
+    print(f"Realizando port scan no alvo: {target}")
 
-# Acrescente o endereço IP que deseja escanear 
-ip_alvo = '[Endereço IP que você quer Scanear]'
-resultado_scan = nmap_scan(ip_alvo)
-print(resultado_scan)
+    nm.scan(target, arguments='-T4')
+
+    scan_results = f"## Resultados do Port Scan para {target}\n\n"
+    for host in nm.all_hosts():
+        scan_results += f"### Host: {host} ({nm[host].hostname()})\n"
+        scan_results += f"*Estado:* {nm[host].state()}\n\n"
+
+        for proto in nm[host].all_protocols():
+            scan_results += f"#### Protocolo: {proto}\n\n"
+
+            lport = nm[host][proto].keys()
+            for port in lport:
+                scan_results += f"- **Porta:** {port}\n\t- **Estado:** {nm[host][proto][port]['state']}\n\t- **Serviço:** {nm[host][proto][port]['name']}\n\n"
+
+    save_scan_results(scan_results, "Descoberta de Portas", f"{target}.md")
+    print(scan_results)
+
+
+def save_scan_results(content, folder_name, file_name):
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+    with open(os.path.join(folder_name, file_name), 'w') as file:
+        file.write(content)
+
+
+if __name__ == "__main__":
+    target = input("Digite o endereço do alvo para o port scan: ")
+    port_scan(target)
